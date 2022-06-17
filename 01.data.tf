8c3e430e331a5fd21c6cf44648efb05fa8b45558
unknown <kimmim1223@naver.com>
2022-06-17 16:00:03 +0900

    22-06-17

--- 01.data.tf
+++ 01.data.tf
@@ -2,7 +2,7 @@
 #   backend "s3" {
 #     bucket         = "hemmu" # bucket name
 #     key            = "terraform/terraform.tfstate"
-#     region         = "ap-northeast-3"
+#     region         = "ap-northeast-2"
 #     dynamodb_table = "terraform-up-and-running-locks" # dynamoDB table name
 #     encrypt        = true
 #   }
@@ -14,7 +14,3 @@ data "terraform_remote_state" "network" {
         path = "C:/Users/김햄무/Desktop/terraform/Aws/@init_network/terraform.tfstate"
     }   
 }
-
-/* 
-terraform init -backend-config="access_key=AKIA5TOVVFHINU67IIWT" -backend-config="secret_key=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"
-*/
