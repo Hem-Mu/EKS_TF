ec02c1cdbbbaace433e5b676247206acd1f7f344
Hem-Mu <93104310+Hem-Mu@users.noreply.github.com>
2022-09-02 13:31:53 +0900

    Update 01.data.tf

--- 01.data.tf
+++ 01.data.tf
@@ -1,13 +1,3 @@
-# terraform {
-#   backend "s3" {
-#     bucket         = "hemmu" # bucket name
-#     key            = "terraform/terraform.tfstate"
-#     region         = "ap-northeast-2"
-#     dynamodb_table = "terraform-up-and-running-locks" # dynamoDB table name
-#     encrypt        = true
-#   }
-# } # depends on S3
-
 data "terraform_remote_state" "network" {
     backend = "local"
     config = {
